a28d31ac5dd7007a1079562f0b92fde6473c716a
IsmailDerkaoui <ismailderkaoui.0@gmail.com>
2025-07-16 00:37:37 +0200

    modifications des imports pour corriger les erreurs Netlify CI build

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
-  DiPython,
   DiGit,
-  DiJava,
 } from "react-icons/di";
-import {
-  SiRedis,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
